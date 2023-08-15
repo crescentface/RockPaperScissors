453d73aa7febf3e9cbbf4f68130e10bec91423a5
crescentface <c_faze@hotmail.co.uk>
2023-08-15 14:16:01 +0100

    Completed funtion to play one round

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-console.log("Hello World!");
-
-const rockPaperScissors = ['Rock', 'Paper', 'Scissors'];
+const rockPaperScissors = ["rock", "paper", "scissors"];
+const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
-    let randomPick = Math.floor(Math.random() * rockPaperScissors.length)
-    return rockPaperScissors[randomPick]
+    return rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+   if (playerSelection === computerSelection) {
+     return "Draw! You both selected " + playerSelection; 
+   } else if (playerSelection === "rock" && computerSelection === "paper") {
+     return "You lose! Paper beats rock!";
+   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+     return "You lose! Scissors beats rock!";
+   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+     return "You lose! Scissors beats paper!";
+   } else {
+     return "You win! " + playerSelection + " beats " + computerSelection + "!";
+   }
+}
 
+console.log(playRound(playerSelection, computerSelection));
