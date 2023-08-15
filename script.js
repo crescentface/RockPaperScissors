43c1b2ae78e269a69d2bebd77c96b98d0b8b4179
crescentface <c_faze@hotmail.co.uk>
2023-08-15 15:57:12 +0100

    Added player and computer score tracker

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 const rockPaperScissors = ["rock", "paper", "scissors"];
 const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
 const computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
+
 
 function getComputerChoice() {
     return rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
 }
 
+
 function playRound(playerSelection, computerSelection) {
-   if (playerSelection === computerSelection) {
-     return "Draw! You both selected " + playerSelection; 
-   } else if (playerSelection === "rock" && computerSelection === "paper") {
-     return "You lose! Paper beats rock!";
-   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-     return "You lose! Scissors beats rock!";
-   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-     return "You lose! Scissors beats paper!";
-   } else {
-     return "You win! " + playerSelection + " beats " + computerSelection + "!";
-   }
+ if (playerSelection === computerSelection) {
+    alert("The current score is: Player: " + playerScore + " vs Computer: " + computerScore);
+    computerScore++;
+    playerScore++;
+    return "Draw! You both selected " + playerSelection; 
+ } else if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore++;
+    alert("The current score is: Player: " + playerScore + " vs Computer: " + computerScore);
+    return "You lose! Paper beats rock!";
+ } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore++;
+    alert("The current score is: Player: " + playerScore + " vs Computer: " + computerScore);
+    return "You lose! Scissors beats rock!";
+ } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore++;
+    alert("The current score is: Player: " + playerScore + " vs Computer: " + computerScore);
+    return "You lose! Scissors beats paper!";
+ } else {
+    playerScore++;
+    alert("The current score is: Player: " + playerScore + " vs Computer: " + computerScore);
+    return "You win! " + playerSelection + " beats " + computerSelection + "!";
+}
+}
+
+function game() {
+    while (playerScore <=5 || computerScore <=5) {
+        playRound(playerSelection, computerSelection);
+      }
 }
 
 console.log(playRound(playerSelection, computerSelection));
