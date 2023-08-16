231f37213b4b51357d24bc202334de759f9d99ff
crescentface <c_faze@hotmail.co.uk>
2023-08-16 08:27:29 +0100

    Added comments to all functions for future ref.

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const rockPaperScissors = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 
-
+//Function to randomise the computer pick of rock, paper, scissors
 function getComputerChoice() {
    return rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
 }
 
+//Gets the winner at the end of the game
 function getWinner() {
    if (playerScore < computerScore) {
      alert ("You lose!");
@@ -22,15 +23,16 @@ function getWinner() {
 }
 
 
-
+//Plays a round of rock, paper, scissors. Alerting players of the current score and
+//who won that round. Could be tidier?
 function playRound(playerSelection, computerSelection)
 {
  playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
  computerSelection = getComputerChoice();
  if (playerSelection === computerSelection) {
-   alert("The current score is: Player: " + playerScore + " vs Computer: " + computerScore);
      computerScore++;
      playerScore++;
+     alert("The current score is: Player: " + playerScore + " vs Computer: " + computerScore);
      return "Draw! You both selected " + playerSelection; 
  } else if (playerSelection === "rock" && computerSelection === "paper") {
      computerScore++;
@@ -51,6 +53,9 @@ function playRound(playerSelection, computerSelection)
   } 
 }
 
+
+//Iterates through i and plays 5 rounds of the game. Try to integrate
+//getWinner() function into this, not worked correctly as of yet.
 function game() {
    for (let i = 0; i < 5; i++) {
         console.log(playRound());     
@@ -58,6 +63,7 @@ function game() {
 }
 
 
-
+//Having to call game then getWinner due to not being able to integrate
+//described in game().
 game();
 getWinner();
